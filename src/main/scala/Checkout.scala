7dce3eaac954a1d773fec9caef9f974f77ac0948
Richard Hague <richhague@gmail.com>
2025-11-11 16:15:52 +0000

    Step 2: Simple Offers

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,16 +1,37 @@
+import Fruit.{Apple, Orange}
+
 import scala.language.postfixOps
+
+enum Fruit(name: String, price: Int):
+  def getPrice: Int = price
+  case Apple extends Fruit("apple", 60)
+  case Orange extends Fruit("orange", 25)
+  case Unknown extends Fruit("unknown", 0)
 
 object Checkout {
 
-  private def getPriceOf(item: String): Int =  Option(item).map(_.toLowerCase) match {
-    case Some("apple") => 60
-    case Some("orange") => 25
-    case _ => 0
+  private def toFruit(item: String): Fruit =  Option(item).map(_.toLowerCase) match {
+    case Some("apple") => Fruit.Apple
+    case Some("orange") => Fruit.Orange
+    case _ => Fruit.Unknown
+  }
+
+  def applyOffer(fruit: Fruit, numberOfFruit: Int, offerThreshold: Int, discount: Int): Int = {
+    val totalPrice = fruit.getPrice * numberOfFruit
+    if(numberOfFruit >= offerThreshold) {
+      totalPrice - ((numberOfFruit / offerThreshold) * (discount * fruit.getPrice))
+    } else{
+      totalPrice
+    }
   }
 
   def scan(seq: Seq[String]): Int = {
     Option(seq) match {
-      case Some(seq) => seq.map(getPriceOf).sum
+      case Some(seq) =>
+        val someFruit = seq.map(toFruit)
+        val numberOfApples = someFruit.count(_ == Apple)
+        val numberOfOranges = someFruit.count(_ == Orange)
+        applyOffer(Apple, numberOfApples, 2, 1) + applyOffer(Orange, numberOfOranges, 3, 1)
       case _ => 0
     }
   }
--- src/test/scala/CheckoutTest.scala
+++ src/test/scala/CheckoutTest.scala
@@ -1,7 +1,9 @@
+import Fruit.{Apple, Orange}
+
 class CheckoutTest extends munit.FunSuite {
   test("checkout with different values") {
     val total = Checkout.scan(List("Apple", "Orange", "oRange", "apple", null, "Duff"))
-    assertEquals(total, 170)
+    assertEquals(total, 110)
   }
 
   test("checkout with no values") {
@@ -12,5 +14,40 @@ class CheckoutTest extends munit.FunSuite {
   test("checkout with null value") {
     val total = Checkout.scan(null)
     assertEquals(total, 0)
+  }
+
+  test("apply offer 3 for 2") {
+    val total = Checkout.applyOffer(Orange, 3, 3, 1)
+    assertEquals(total, 2 * Orange.getPrice)
+  }
+
+  test("apply offer 3 for 2 with extra fruit") {
+    val total = Checkout.applyOffer(Orange, 4, 3, 1)
+    assertEquals(total, 3 * Orange.getPrice)
+  }
+  
+  test("apply offer 3 for 2 with 2 extra fruit") {
+    val total = Checkout.applyOffer(Orange, 5, 3, 1)
+    assertEquals(total, 4 * Orange.getPrice)
+  }
+
+  test("apply offer 3 for 2 with double the needed") {
+    val total = Checkout.applyOffer(Orange, 6, 3, 1)
+    assertEquals(total, 4 * Orange.getPrice)
+  }
+
+  test("apply offer buy one get one free") {
+    val total = Checkout.applyOffer(Apple, 2, 2, 1)
+    assertEquals(total, Apple.getPrice)
+  }
+
+  test("apply offer buy one get one free with extra fruit") {
+    val total = Checkout.applyOffer(Apple, 3, 2, 1)
+    assertEquals(total, 2 * Apple.getPrice)
+  }
+
+  test("apply offer buy one get one free with double the fruit needed") {
+    val total = Checkout.applyOffer(Apple, 4, 2, 1)
+    assertEquals(total, 2 * Apple.getPrice)
   }
 }
